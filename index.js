4511d1d1ae04ad1b4ef0471ce1cc35de71f7890e
Erin Green <erin.y.green@gmail.com>
2020-06-24 20:10:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
 
 append.push("Odie")
-prepend.unshift("Coco")
+prepend.unshift("Odie")
 removeLast.pop()
 removeFirst.shift()
